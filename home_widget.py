04507588b2758946700c3eef55c123a1b9d0ded7
박재현님/안전보건기획팀_SK오앤에스 <P124162@sktelecom.com>
2024-05-20 14:27:29 +0900

    보안 취약점 조치2

--- home_widget.py
+++ home_widget.py
@@ -5,16 +5,47 @@ import os
 import plotly.graph_objects as go
 import plotly.io as pio
 from PyQt5.QtCore import Qt, QDateTime
-from PyQt5.QtGui import QPainter, QColor, QBrush
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QToolTip
+from PyQt5.QtGui import QPainter, QColor, QBrush, QPixmap, QFont
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QToolTip, QLabel
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import pyqtgraph as pg
 from PyQt5.QtChart import QBarSet, QBarSeries, QChart, QBarCategoryAxis, QValueAxis, QChartView, QLineSeries, \
     QDateTimeAxis
 from datetime import datetime
-from main import ScoreLabel
+# from main import ScoreLabel
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
+
+
+class ScoreLabel(QLabel):
+    def __init__(self, score, image_path, background_color, parent=None):
+        super().__init__(parent)
+        self.score = score
+        self.image_path = image_path
+        self.background_color = background_color
+        self.setAlignment(Qt.AlignCenter)
+        self.setFixedSize(200, 400)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        # 배경색 채우기
+        painter.fillRect(self.rect(), self.background_color)
+        # 이미지 그리기
+        if self.image_path:
+            image = QPixmap(self.image_path).scaled(200, 100, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            # 이미지 위치를 중앙에 맞추기 위한 계산
+            iw, ih = image.width(), image.height()
+            ix, iy = (self.width() - iw) // 2, (self.height() - ih) // 2
+            iy -= 90 # 이미지를 위로 10픽셀 이동
+            painter.drawPixmap(ix, iy, iw, ih, image)
+        # 텍스트 그리기
+        painter.setPen(QColor('white'))
+        painter.setFont(QFont('Arial', 8, QFont.Bold))  # 폰트 크기를 조정하여 상단 텍스트에 적용
+        painter.drawText(self.rect().adjusted(0, 50, 0, 0), Qt.AlignCenter, "최근 PC 점검 점수")  # 상단 텍스트 위치 조정
+        painter.setFont(QFont('Arial', 20, QFont.Bold))
+        # painter.drawText(self.rect(), Qt.AlignCenter, f"{self.score}점")
+        painter.drawText(self.rect().adjusted(0, 100, 0, 0), Qt.AlignCenter, f"{self.score}점")
+        painter.end()
 
 class HomeWidget(QWidget):
     def __init__(self):
